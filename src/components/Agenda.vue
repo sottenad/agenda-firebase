bf58969b8175274fd1c7680b926ee2538578b78f
Steve Ottenad <steveo@bluerooster.com>
2018-08-20 18:35:09 -0700

    Change

--- src/components/Agenda.vue
+++ src/components/Agenda.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="login">
     <h3>Agenda</h3>
-    <p>Welcome to the agenda page</p>
+    <p>Welcome to the agenda page.</p>
     <p><a v-on:click="logOut">Log Out</a></p>
   </div>
 </template>
